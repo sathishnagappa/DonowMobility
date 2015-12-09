f45e5bdb1d8ea1212af3732d9ddd4dd889a9cf71
vaibhavBrillio <vaibhav.barchhiha@brillio.com>
2015-12-09 20:59:05 +0530

    Added Lead Update and Lead Interaction feedback page

--- iOS/LandingLeadsVC.designer.cs
+++ iOS/LandingLeadsVC.designer.cs
@@ -16,6 +16,10 @@ namespace donow.iOS
 	{
 		[Outlet]
 		[GeneratedCode ("iOS Designer", "1.0")]
+		UIView AlertView { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
 		UIButton ButtonRequestNewLead { get; set; }
 
 		[Outlet]
@@ -24,6 +28,10 @@ namespace donow.iOS
 
 		void ReleaseDesignerOutlets ()
 		{
+			if (AlertView != null) {
+				AlertView.Dispose ();
+				AlertView = null;
+			}
 			if (ButtonRequestNewLead != null) {
 				ButtonRequestNewLead.Dispose ();
 				ButtonRequestNewLead = null;
