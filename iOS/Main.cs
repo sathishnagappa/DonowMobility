cbfe74656d006b9cb919c2632363f02ce74a204b
PriyabrataDash <priyabrata.dash@brillio.com>
2015-12-21 12:50:05 +0530

    Insight Packages added.

--- iOS/Main.cs
+++ iOS/Main.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Foundation;
 using UIKit;
 
@@ -9,10 +9,10 @@ namespace donow.iOS
 		// This is the main entry point of the application.
 		static void Main (string[] args)
 		{
+			Xamarin.Insights.Initialize (XamarinInsights.ApiKey);
 			// if you want to use a different Application Delegate class from "AppDelegate"
 			// you can specify it here.
 			UIApplication.Main (args, null, "AppDelegate");
-
 		}
 	}
 }
